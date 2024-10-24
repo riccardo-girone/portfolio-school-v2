2492baabc481e2ca9d0929839935a2599e92d076
riccardo-girone <149433192+riccardo-girone@users.noreply.github.com>
2024-10-24 23:21:13 +0200

    fix: js small fix

--- app.js
+++ app.js
@@ -9,9 +9,17 @@ for (let i = 0; i < columns; i++) {
 	for (let j = 0; j < squarePerColumn; j++) {
 		const square = document.createElement("div");
 		square.classList.add("square");
+		square.style.height = squareHeight() + "px";
 		column.appendChild(square);
 	}
 
 	console.log("boboz");
 	grid.appendChild(column);
+}
+
+function squareHeight() {
+	const windowHeight = window.innerHeight;
+	let height = windowHeight / squarePerColumn;
+
+	return height;
 }
